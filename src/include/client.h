2d3499dd3435e05741ef5eec04fce90f0bea96fc
Chengxi <cx.xie@sjtu.edu.cn>
2024-10-07 01:56:12 +0800

    1写成了-1导致效率降低10倍，我傻了

--- src/include/client.h
+++ src/include/client.h
@@ -7,6 +7,7 @@
 #include <utility>
 #include <vector>
 #include <queue>
+#include <chrono>
 
 extern int rows;         // The count of rows of the game map.
 extern int columns;      // The count of columns of the game map.
@@ -20,12 +21,13 @@ struct visit_info
   int column;
   int unvisited;
   int marked_count;
+  int around;
 };
 
 struct op_compare
 {
   bool operator() (visit_info & a, visit_info & b) {
-    return 0.34*a.marked_count-a.unvisited < 0.34*b.marked_count-b.unvisited;
+    return 0.2*a.marked_count-a.unvisited+0.34*a.around < 0.2*b.marked_count-b.unvisited+0.34*b.around;
   }
 };
 
@@ -44,8 +46,12 @@ std::vector <std::vector<int>> cli_game_map;
 std::vector <operation> immediate;
 int remaining_mines;
 int remaining_blocks;
-int width = 10;
-int depth = 5;
+int width;
+int depth;
+int time_limit;
+decltype(std::chrono::high_resolution_clock::now()) start_time;
+bool first = true;
+
 
 
 std::vector<std::vector<int>> copy (std::vector<std::vector<int>> &map) {
@@ -299,6 +305,18 @@ void InitGame() {
   cli_game_map = std::vector<std::vector<int>>(rows, std::vector<int>(columns));
   immediate = std::vector <operation>();
 
+  width = 20;
+  depth = 6;
+  if (rows==10&&columns==10) {
+    time_limit = 5000;
+  }
+  else{
+    time_limit = 30000;
+  }
+  if (first) {
+    start_time = std::chrono::high_resolution_clock::now();
+    first = false;
+  }
 
   int first_row, first_column;
   std::cin >> first_row >> first_column;
@@ -348,7 +366,7 @@ void Decide() {
   // TODO (student): Implement me!
   // while (true) {
   //   Execute(0, 0);
-  //   记得Excute之后Return;
+  //   记得Execute之后Return;
   // }
   std::priority_queue<visit_info, std::vector<visit_info>, op_compare> visit;
 
@@ -361,40 +379,48 @@ void Decide() {
 
   for (int r=0; r<rows; r++) {
     for (int c=0; c< columns; c++) {
-      int count = 0, marked_count = 0;
+      int count = 0, marked_count = 0, total = 0;
       if (r > 0 && c > 0) {
         if (cli_game_map[r-1][c-1] == -3) marked_count++;
-        if (cli_game_map[r-1][c-1] == -4) count++;
+        else if (cli_game_map[r-1][c-1] == -4) count++;
+        else total += cli_game_map[r-1][c-1];
       }
       if (r > 0) {
         if (cli_game_map[r-1][c] == -3) marked_count++;
-        if (cli_game_map[r-1][c] == -4) count++;
+        else if (cli_game_map[r-1][c] == -4) count++;
+        else total += cli_game_map[r-1][c];
       }
       if (r > 0 && c < columns-1) {
         if (cli_game_map[r-1][c+1] == -3) marked_count++;
-        if (cli_game_map[r-1][c+1] == -4) count++;
+        else if (cli_game_map[r-1][c+1] == -4) count++;
+        else total += cli_game_map[r-1][c+1];
       }
       if (c > 0) {
         if (cli_game_map[r][c-1] == -3) marked_count++;
-        if (cli_game_map[r][c-1] == -4) count++;
+        else if (cli_game_map[r][c-1] == -4) count++;
+        else total += cli_game_map[r][c-1];
       }
       if (c < columns-1) {
         if (cli_game_map[r][c+1] == -3) marked_count++;
-        if (cli_game_map[r][c+1] == -4) count++;
+        else if (cli_game_map[r][c+1] == -4) count++;
+        else total += cli_game_map[r][c+1];
       }
       if (r < rows-1 && c > 0) {
         if (cli_game_map[r+1][c-1] == -3) marked_count++;
-        if (cli_game_map[r+1][c-1] == -4) count++;
+        else if (cli_game_map[r+1][c-1] == -4) count++;
+        else total += cli_game_map[r+1][c-1];
       }
       if (r < rows-1) {
         if (cli_game_map[r+1][c] == -3) marked_count++;
-        if (cli_game_map[r+1][c] == -4) count++;
+        else if (cli_game_map[r+1][c] == -4) count++;
+        else total += cli_game_map[r+1][c];
       }
       if (r < rows-1 && c < columns-1) {
         if (cli_game_map[r+1][c+1] == -3) marked_count++;
-        if (cli_game_map[r+1][c+1] == -4) count++;
+        else if (cli_game_map[r+1][c+1] == -4) count++;
+        else total += cli_game_map[r+1][c+1];
       }
-      if (count>0 && count == cli_game_map[r][c] - marked_count) {
+      if (count > 0 && count == cli_game_map[r][c] - marked_count) {
         if (r > 0 && c > 0) {
           if (cli_game_map[r-1][c-1] == -4) {
             immediate.push_back(operation{r-1, c-1, 1});
@@ -452,19 +478,23 @@ void Decide() {
           Execute(r, c, 0);
           return;
         }
-        visit.push(visit_info({r, c, count, marked_count}));
+        visit.push(visit_info({r, c, count, marked_count, total}));
       }
     }
   }
   int size = visit.size();
   visit_info root;
-  for (int i=0; i<std::min(size, width); i++) {
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-start_time).count();
+  if (duration + 200 < time_limit) {
+  for
+   (int i=0; i<std::min(size, width); i++) {
     root = visit.top();
     visit.pop();
     auto temp_map = copy(cli_game_map);
     if (remaining_mines > remaining_blocks/2) {
       if (!assume(temp_map, root.row, root.column, -1, depth)) {
-        Execute(root.row, root.column, -1);
+        Execute(root.row, root.column, 1);
         return;
       }
       if (!assume(temp_map, root.row, root.column, -3, depth)) {
@@ -478,12 +508,12 @@ void Decide() {
         return;
       }
       if (!assume(temp_map, root.row, root.column, -1, depth)) {
-        Execute(root.row, root.column, -1);
+        Execute(root.row, root.column, 1);
         return;
       }
     }
   }
-
+  }
   if (!visit.empty()) {
     root = visit.top();
     if (remaining_mines > remaining_blocks/2) {
@@ -494,7 +524,6 @@ void Decide() {
       Execute(root.row, root.column, 0);
       return;
     }
-    return;
   }
 
   if (size > 0) {
@@ -506,7 +535,6 @@ void Decide() {
       Execute(root.row, root.column, 0);
       return;
     }
-    return;
   }
 
   Execute(0, 0, 2);
